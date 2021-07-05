e64e8e50cb583a9f94b2ba4706881b0f30a8328e
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2021-07-05 15:08:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ function pauseMusic() {
   wavesurfer.pause();
 }
 
-wavesurfer.load("https://file06.mp3ring.org/dl/iflgzZNCTY6AIfMbiTHy3A/1625476994/best-ringtone-download-044f4738dcc275b056ffc492530a5bf5/just-like-animals-maroon-5.mp3");
+wavesurfer.load("./animals.mp3");
